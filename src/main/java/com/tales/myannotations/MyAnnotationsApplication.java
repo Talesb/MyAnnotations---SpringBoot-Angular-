c4a5e13cc0a4d749936f8c4fe3ffa316738c540f
talesb <talesbatistafig@hotmail.com>
2018-04-11 18:42:46 -0300

    fist commit

---
+++ src/main/java/com/tales/myannotations/MyAnnotationsApplication.java
@@ -0,0 +1,12 @@
+package com.tales.myannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnnotationsApplication.class, args);
+	}
+}
